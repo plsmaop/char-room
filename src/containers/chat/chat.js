0aee794bc42111c2f445fa447c83583a03ba89a5
plsmaop <allenivan@gmail.com>
2018-05-23 16:05:37 +0800

    keep working on RWD

--- src/containers/chat/chat.js
+++ src/containers/chat/chat.js
@@ -4,13 +4,30 @@ import '../../style/style.css';
 import EnterMsg from '../enterMsg';
 import ChatHistory from '../chatHistory';
 
-const Chat = () => (
-  <div className="chat" >
-    <Paper zDepth={2} >
-      <ChatHistory />
-      <EnterMsg />
-    </Paper>
-  </div>
-);
+class Chat extends React.Component {
+  componentDidMount() {
+    this.scrollToBottom();
+  }
+  componentDidUpdate() {
+    this.scrollToBottom();
+  }
+  scrollToBottom() {
+    this.messagesEnd.scrollIntoView();
+  }
+  render() {
+    return (
+      <div className="chat" >
+        <Paper zDepth={2} >
+          <ChatHistory />
+          <EnterMsg />
+        </Paper>
+        <div
+          style={{ float: 'left', clear: 'both' }}
+          ref={(el) => { this.messagesEnd = el; }}
+        />
+      </div>
+    );
+  }
+}
 
 export default Chat;
